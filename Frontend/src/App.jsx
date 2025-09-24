ab289a382fe47d9aeec963b04b3d1aec5445448d
Kingdood-debug <rishisharma5600@gmail.com>
2025-09-24 14:49:51 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,7 +3,7 @@ import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import Customize from "./pages/Customize";
 import { useContext } from "react";
-import { userDataContext } from "./context/userContext.jsx";
+import { userDataContext } from "./context/UserContext.jsx";
 import Home from "./pages/Home.jsx";
 import Customize2 from "./pages/Customize2.jsx";
 
